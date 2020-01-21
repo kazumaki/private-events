a4b7c45623b1fe1b233e1351415a912ae6a49ef8
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-01-21 13:00:45 -0300

    Build user model; Create user and session controllers; Create signin/signup forms

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  helper_method :current_user
+
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    if user
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to login_path
+  end
+end
