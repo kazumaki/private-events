761930c5d83584271149296f13ea2b5eeb606e3e
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-01-23 15:19:20 -0300

    Fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,19 +1,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require "capybara/rspec"
+require 'capybara/rspec'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
+# in _spec.rb will both be required and run as specs, ' the specs to be
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
@@ -60,5 +60,5 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  # config.filter_gems_from_backtrace('gem name')
 end
--- spec/users_signin_integration_test_spec.rb
+++ spec/users_signin_integration_test_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'User login', type: :feature do
   scenario 'With valid login data' do
-    name = "Valid"
+    name = 'Valid'
     visit login_path
     User.create(name: name)
     fill_in 'Name', with: name
-    click_button "Log-in"
+    click_button 'Log-in'
     expect(page).to have_content('Created events')
   end
 
   scenario 'With invalid login data' do
     visit login_path
-    fill_in 'Name', with: ""
-    click_button "Log-in"
+    fill_in 'Name', with: ''
+    click_button 'Log-in'
     expect(page).to have_content('Login')
   end
 end
